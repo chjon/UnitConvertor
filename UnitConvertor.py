bb4697bb654abf4d364e7b8647513ecbc23e78bf
Jonathan Chung <jt2chung@uwaterloo.ca>
2020-12-13 01:00:10 -0500

    implement unit conversion reading/writing

--- UnitConvertor.py
+++ UnitConvertor.py
@@ -1,5 +1,61 @@
 from Unit import *
 
+class FileFormatError(Exception): pass
+
+L1_DELIMITER = ';'
+L2_DELIMITER = ','
+L3_DELIMITER = ' '
+
+def loadConversions(filename):
+	units = {}
+	conversions = {}
+	file = open(filename, 'r')
+	lines = [ line[0:-1] for line in file.readlines() ]
+	for line in lines:
+		components = line.split(L1_DELIMITER)
+		if len(components) == 1:
+			# Create base unit
+			sym = components[0]
+			if not sym.isalpha():
+				raise FileFormatError(f"Improper format: units must be alphabetical, received '{sym}'")
+			units[sym] = Unit(sym)
+		elif len(components) == 3:
+			# Create derived unit
+			sym = components[0]
+			if not sym.isalpha():
+				raise FileFormatError(f"Improper format: units must be alphabetical, received '{sym}'")
+			val = float(components[1])
+			baseUnits = {}
+			for baseUnitStr in components[2].split(L2_DELIMITER):
+				baseUnitComponents = baseUnitStr.split(L3_DELIMITER)
+				if len(baseUnitComponents) != 2:
+					raise FileFormatError(f"Improper format: expected 2 components, received '{baseUnitStr}'")
+				baseUnit, exponent = baseUnitComponents
+				if not baseUnit.isalpha():
+					raise FileFormatError(f"Improper format: units must be alphabetical, received '{baseUnit}'")
+				baseUnits[baseUnit] = int(exponent)
+			units[sym] = Unit(sym, baseUnits)
+			conversions[sym] = val
+		else:
+			print(components)
+			raise FileFormatError(f"Improper format: expected 3 components, received {len(components)}")
+
+	file.close()
+	return units, conversions
+
+def writeConversions(filename, units, conversions):
+	file = open(filename, 'w')
+	for sym, unit in units.items():
+		if len(unit.baseUnits) == 0:
+			file.write(f"{sym}\n")
+		else:
+			baseUnitStr = ''
+			for baseUnitSym, exponent in unit.baseUnits.items():
+				baseUnitStr += f"{baseUnitSym}{L3_DELIMITER}{exponent}{L2_DELIMITER}"
+			file.write(f"{unit.sym}{L1_DELIMITER}{conversions[unit.sym]}{L1_DELIMITER}{baseUnitStr[0:-1]}\n")
+
+	file.close()
+
 class Convertor:
 	# Topological sort implemented using DFS
 	def topologicalSortVisit(self, derivedUnit: str, visited: set):
@@ -23,4 +79,5 @@ class Convertor:
 	def __init__(self, units = {}, conversions = {}):
 		self.units = units
 		self.conversions = conversions
+		self.topology = []
 		self.topologicalSort()
--- convertor_test.py
+++ convertor_test.py
@@ -4,48 +4,71 @@ from UnitConvertor import *
 def test_conversion(verbose = False):
 	test_result = 0
 
-	units = {
-		"m"  : Unit("m"  ),
-		"g"  : Unit("g"  ),
-		"s"  : Unit("s"  ),
-		"A"  : Unit("A"  ),
-		"K"  : Unit("K"  ),
-		"mol": Unit("mol"),
-		"cd" : Unit("cd" ),
-		"N"  : Unit("N"  , {"m" : +1, "g": +1, "s": -2}),
-		"Hz" : Unit("Hz" , {"s" : -1}),
-		"J"  : Unit("J"  , {"N" : +1, "m": +1}),
-		"Pa" : Unit("Pa" , {"N" : +1, "m": +2}),
-		"W"  : Unit("W"  , {"J" : +1, "s": -1}),
-		"C"  : Unit("C"  , {"s" : +1, "A": +1}),
-		"V"  : Unit("V"  , {"W" : +1, "s": -1}),
-		"F"  : Unit("F"  , {"C" : +1, "V": -1}),
-		"Ohm": Unit("Ohm", {"V" : +1, "A": -1}),
-		"S"  : Unit("S"  , {"A" : +1, "V": -1}),
-		"Wb" : Unit("Wb" , {"V" : +1, "s": +1}),
-		"T"  : Unit("T"  , {"Wb": +1, "m": -2}),
-		"H"  : Unit("H"  , {"Wb": +1, "A": -1}),
-		"Bq" : Unit("Bq" , {"s" : -1}),
-		"Gy" : Unit("Gy" , {"J" : +1, "g": -1}),
-	}
+	# units = {
+	# 	# SI and SI derived
+	# 	"m"  : Unit("m"  ),
+	# 	"g"  : Unit("g"  ),
+	# 	"s"  : Unit("s"  ),
+	# 	"A"  : Unit("A"  ),
+	# 	"K"  : Unit("K"  ),
+	# 	"mol": Unit("mol"),
+	# 	"cd" : Unit("cd" ),
+	# 	"N"  : Unit("N"  , {"m" : +1, "g": +1, "s": -2}),
+	# 	"Hz" : Unit("Hz" , {"s" : -1}),
+	# 	"J"  : Unit("J"  , {"N" : +1, "m": +1}),
+	# 	"Pa" : Unit("Pa" , {"N" : +1, "m": +2}),
+	# 	"W"  : Unit("W"  , {"J" : +1, "s": -1}),
+	# 	"C"  : Unit("C"  , {"s" : +1, "A": +1}),
+	# 	"V"  : Unit("V"  , {"W" : +1, "s": -1}),
+	# 	"F"  : Unit("F"  , {"C" : +1, "V": -1}),
+	# 	"Ohm": Unit("Ohm", {"V" : +1, "A": -1}),
+	# 	"S"  : Unit("S"  , {"A" : +1, "V": -1}),
+	# 	"Wb" : Unit("Wb" , {"V" : +1, "s": +1}),
+	# 	"T"  : Unit("T"  , {"Wb": +1, "m": -2}),
+	# 	"H"  : Unit("H"  , {"Wb": +1, "A": -1}),
+	# 	"Bq" : Unit("Bq" , {"s" : -1}),
+	# 	"Gy" : Unit("Gy" , {"J" : +1, "g": -1}),
 
-	conversions = {
-		"N"  : 1000,
-		"Hz" :    1,
-		"J"  :    1,
-		"Pa" :    1,
-		"W"  :    1,
-		"C"  :    1,
-		"V"  :    1,
-		"F"  :    1,
-		"Ohm":    1,
-		"S"  :    1,
-		"Wb" :    1,
-		"T"  :    1,
-		"H"  :    1,
-		"Bq" :    1,
-		"Gy" :    1,
-	}
+	# 	# Non-SI
+	# 	"in" : Unit("in" , {"m"  : 1}),
+	# 	"ft" : Unit("ft" , {"in" : 1}),
+	# 	"yd" : Unit("yd" , {"ft" : 1}),
+	# 	"mi" : Unit("mi" , {"yd" : 1}),
+	# 	"min": Unit("min", {"s"  : 1}),
+	# 	"hr" : Unit("hr" , {"min": 1}),
+	# 	"mph": Unit("mph", {"mi" : 1, "hr": -1})
+	# }
+
+	# conversions = {
+	# 	# SI derived
+	# 	"N"  : 1000,
+	# 	"Hz" :    1,
+	# 	"J"  :    1,
+	# 	"Pa" :    1,
+	# 	"W"  :    1,
+	# 	"C"  :    1,
+	# 	"V"  :    1,
+	# 	"F"  :    1,
+	# 	"Ohm":    1,
+	# 	"S"  :    1,
+	# 	"Wb" :    1,
+	# 	"T"  :    1,
+	# 	"H"  :    1,
+	# 	"Bq" :    1,
+	# 	"Gy" :    1,
+
+	# 	# Non-SI
+	# 	"in" : 0.0254,
+	# 	"ft" : 1/  12,
+	# 	"yd" : 1/   3,
+	# 	"mi" : 1/1760,
+	# 	"min": 1/  60,
+	# 	"hr" : 1/  60,
+	# 	"mph": 1
+	# }
+
+	units, conversions = loadConversions("test.csv")
+	writeConversions("test2.csv", units, conversions)
 
 	# Test topological sort
 	convertor = Convertor(units, conversions)
