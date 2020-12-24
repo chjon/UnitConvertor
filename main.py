6608ed42b26bca0344a581120de00815ebe0b92a
Jonathan Chung <jt2chung@uwaterloo.ca>
2020-12-23 21:45:37 -0500

    Polish user interface

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ COMMAND_EXIT = "exit"
 COMMAND_HELP = "help"
 COMMAND_LOAD = "load"
 COMMAND_SAVE = "save"
-units, conversions, prefixes = {}, {}, {}
+convertor = UC_Convertor.Convertor({}, {}, {})
+INDENT = " -> "
 
 def command_help(args):
 	helpStrings = {
@@ -21,24 +22,34 @@ def command_help(args):
 	inputExamples = [
 		"60 mph : m/s",
 		"100 kg * 9.8 m/s^2 : N",
-		"500 N / 12 mm^2 : kPA",
+		"500 N / 12 mm^2 : kPa",
 		"123.4 lb / (5 ft + 6 in)^2 : BMI",
 	]
 
 	print("--------------------------")
 	print("Available commands:")
-	for command, helpString in helpStrings.items(): print(f"   {command}: {helpString}")
+	for command, helpString in helpStrings.items(): print(f"{INDENT}{command}: {helpString}")
 	print("Example input:")
-	for example in inputExamples: print(f"   {example}")
+	for example in inputExamples: print(f"{INDENT}{example}")
 	print("--------------------------")
 
 def command_load(args):
 	if len(args) != 2: print("Usage: load <filename>")
-	else: units, conversions, prefixes = UC_FileIO.loadFile(args[1])
+	else:
+		try:
+			units, conversions, prefixes = UC_FileIO.loadFile(args[1])
+			global convertor
+			convertor = UC_Convertor.Convertor(units, conversions, prefixes)
+			print(f"Successfully loaded definitions from '{args[1]}'")
+		except: print(f"Encountered error while loading from '{args[1]}'")
 
 def command_save(args):
 	if len(args) != 2: print("Usage: save <filename>")
-	else: UC_FileIO.writeFile(args[1], units, conversions, prefixes)
+	else:
+		try:
+			UC_FileIO.writeFile(args[1], convertor.units, convertor.conversions, convertor.prefixes)
+			print(f"Successfully saved definitions to '{args[1]}'")
+		except: print(f"Encountered error while saving to '{args[1]}'")
 
 commands = {
 	COMMAND_EXIT: (lambda args: exit()),
@@ -48,19 +59,20 @@ commands = {
 }
 
 def main():
-	units, conversions, prefixes = UC_FileIO.loadFile("standard.csv")
-
+	command_load(["load","standard.csv"])
 	print(f"Type '{COMMAND_HELP}' for a list of commands")
 	for line in fileinput.input():
-		command = line.strip().split()
+		line = line.strip()
+		command = line.split()
 		if command and (command[0] in commands): commands[command[0]](command)
-		else:
+		elif line:
 			try:
 				ast = UC_StrParser.parse(line)
 				print(f"Interpreting input as: '{str(ast)}'")
-				print(f"{str(ast.evaluate())}")
+				print(f"{INDENT}{str(ast.evaluate(convertor))}")
 			except UnitError as err:
-				print(f"Error: {err}")
+				print(f"{INDENT}Error: {err}")
+			finally: print()
 
 if (__name__ == "__main__"):
 	main()
